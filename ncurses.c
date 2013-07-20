9026690cf7cd5c53aff55bdad207a06c1ce2feb1
Florian Margaine <florian@margaine.com>
2013-07-20 13:09:36 +0200

    Using another approach

--- ncurses.c
+++ ncurses.c
@@ -1,39 +1,32 @@
 #include <curses.h>
-#include <stdlib.h>
-
-typedef struct Win
-{
-    WINDOW *win;
-    int x;
-    int y;
-} Win;
 
 void initncurses();
-Win *create_win(const int y, const int x);
-Win *create_win_struct(WINDOW *win, int y, int x);
-void cleanup_wins(Win *wins[COLS][LINES]);
 
-const int cell_cols = 1;
-const int cell_lines = 1;
+const int cell_cols = 2;
+const int cell_lines = 2;
 
 int main(int argc, char *argv[])
 {
-    int i, j;
-    Win *wins[COLS][LINES];
+    int i;
 
     initncurses();
 
-    for (i = 0; i < LINES; i++)
+    // Draw horizontal lines
+    for (i = 0; i < LINES; i += cell_lines)
     {
-        for (j = 0; j < COLS; j++)
-        {
-            wins[i][j] = create_win(i, j);
-        }
+        mvhline(i, 0, ACS_HLINE, COLS);
     }
 
-    wgetch(wins[0][0]->win);
+    // Draw vertical lines
+    for (i = 0; i < COLS; i += cell_cols)
+    {
+        mvvline(0, i, ACS_VLINE, LINES);
+    }
+
+    refresh();
+    getch();
+
     endwin();
-    cleanup_wins(wins);
 
     return 0;
 }
@@ -44,40 +37,4 @@ void initncurses()
     clear();
     noecho();
     cbreak();
-}
-
-Win *create_win(const int y, const int x)
-{
-    WINDOW *local_win;
-
-    local_win = newwin(cell_lines, cell_cols, y, x);
-    box(local_win, 0, 0);
-    wrefresh(local_win);
-
-    return create_win_struct(local_win, y, x);
-}
-
-Win *create_win_struct(WINDOW *win, int y, int x)
-{
-    Win *win_struct = malloc(sizeof(Win));
-
-    win_struct->win = win;
-    win_struct->x = x;
-    win_struct->y = y;
-
-    return win_struct;
-}
-
-void cleanup_wins(Win *wins[COLS][LINES])
-{
-    int i, j;
-
-    for (i = 0; i < LINES; i++)
-    {
-        for (j = 0; j < COLS; j++)
-        {
-            delwin(wins[i][j]->win);
-            free(wins[i][j]);
-        }
-    }
 }
